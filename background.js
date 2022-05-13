39b9ae4d85611f8ec5389627b9c6a2697c3e4e72
Natarajan Krishnaswami <natarajan@krishnaswami.org>
2022-05-12 21:09:18 -0400

    Add context menu support for tab groups

--- background.js
+++ background.js
@@ -2,20 +2,24 @@ import {GetLinksFromSelection, MakeTabsForLinks} from './utils.js';
 
 const kCurWindowMenuItemId = 'open-selected-links-cur-window';
 const kNewWindowMenuItemId = 'open-selected-links-new-window';
+const kNewTabGroupMenuItemId = 'open-selected-links-new-tab-group';
 
 const OpenLinksInSelection = async function(info, tab) {
   console.log('Got menu click: ', info.menuItemId);
-  let windowId;
+  let windowId, tabGroupName;
   if (info.menuItemId === kCurWindowMenuItemId) {
     windowId = chrome.windows.WINDOW_ID_CURRENT;
   } else if (info.menuItemId === kNewWindowMenuItemId) {
     windowId = chrome.windows.WINDOW_ID_NONE;
+  } else if (info.menuItemId === kNewTabGroupMenuItemId) {
+    windowId = chrome.windows.WINDOW_ID_CURRENT;
+    tabGroupName = "New Tab Group";
   } else {
     // Not our circus, not our monkeys.
     return;
   }
   const {links} = await GetLinksFromSelection(tab.id, info.frameId);
-  await MakeTabsForLinks(links, windowId);
+  await MakeTabsForLinks(links, windowId, tabGroupName);
 }
 
 chrome.runtime.onInstalled.addListener(async () => {
@@ -35,6 +39,16 @@ chrome.runtime.onInstalled.addListener(async () => {
     title: 'Open all selected links in the current window',
     visible: true,
   }, ()=>{console.log('Added cur-window menu item')});
+
+  if (chrome.tabGroups !== undefined) {
+    await chrome.contextMenus.create({
+      id: kNewTabGroupMenuItemId,
+      contexts: ["selection"],
+      type: 'normal',
+      title: 'Open all selected links in a new tab group',
+      visible: true,
+    }, ()=>{console.log('Added new-tab-group menu item')});
+  }
 });
 
 chrome.contextMenus.onClicked.addListener(OpenLinksInSelection);
